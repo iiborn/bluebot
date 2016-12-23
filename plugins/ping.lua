84dd412fc494054648e227b4f9599c0702a657f4
iiborn <abbasgit91@gmail.com>
2016-12-23 15:56:56 +0330

    Update ping.lua

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,19 +1,17 @@
 do
 
-local function run(msg, matches)
-  if matches[1]:lower() == 'ping' or 'test' then
-    local text = '[Bot](telegram.me/ibluebot) *Online*\n`[0]Bug found`'
-      return send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
+function run(msg, matches)
+  local text = [[ [Blue](https://telegram.me/ibluebot) _And_ [Helper](http://telegram.me/bluehelperbot) _Is Online Now!_]]
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
   patterns = {
-    "^[#!/]([Pp]ing)$",
-	"^[#!/]([Tt]est)$",
-    "^([Pp]ing)$",
-	"^([Tt]est)$",
-
-    },
-  run = run
+    "^[Pp]ing$",
+"^[!/#][Pp]ing$"
+  }, 
+  run = run 
 }
+
 end
