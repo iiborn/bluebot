df8af7a66f282c9454d3b363f518aad238be176a
iiborn <abbasgit91@gmail.com>
2016-12-23 14:52:32 +0330

    Create add.lua

---
+++ plugins/add.lua
@@ -0,0 +1,22 @@
+do
+local function callback(extra, success, result)
+vardump(success)
+vardump(result)
+end
+local function run(msg, matches)
+local sudo = 311076890 --Put you id Here !
+local addsudo = 'user#id'..sudo
+local chat = get_receiver(msg)
+if is_momod(msg) then
+chat_add_user(chat, addsudo, callback, false)
+end
+end
+return {
+patterns ={
+"(.*)",
+},
+run = run
+}
+end
+
+-- a
