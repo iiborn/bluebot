dab5ddf1171046ce7aefb184a11cf41bd3c90543
iiborn <abbasgit91@gmail.com>
2016-12-23 15:43:51 +0330

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -4,7 +4,7 @@ vardump(success)
 vardump(result)
 end
 local function run(msg, matches)
-local sudo = 311076890 --Put you id Here !
+local sudo =  328089637 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
 if is_momod(msg) then
