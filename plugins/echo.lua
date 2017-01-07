7a5528eaf537a47b7871d36ccc3a83af65edfc1c
iiborn <abbasgit91@gmail.com>
2017-01-07 15:26:35 +0330

    Create echo.lua

---
+++ plugins/echo.lua
@@ -0,0 +1,17 @@
+local function run(msg,matches)
+local text = matches[1]
+  send_api_msg(msg, get_receiver_api(msg),`-------[..text..]`, true, 'md')
+end
+return {
+description = "text to markdown",
+usage = {
+"!markdown *text* : text to bold",
+"!markdown [text](link) : link to hyperlinkhyperlink",
+"!markdown _text_ : text to italic",
+},
+patterns = {
+"^[!/]echo (.*)",
+
+},
+run = run 
+}
